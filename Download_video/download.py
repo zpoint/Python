c866379d9bb35c4d33f42a760629c9e3288eed88
zpoint <zp@zp0int.com>
2016-12-11 13:38:24 +0800

    asyncio aiohttp with semophere

--- Download_video/download.py
+++ Download_video/download.py
@@ -2,9 +2,14 @@ import re
 import asyncio
 import aiohttp
 
-CONCURRENT_REQUESTS = 3
+"""
+It's a template, change it as your will
+"""
+
+
+CONCURRENT_REQUESTS = 8
 headers = {
-    "Host": "t.cn",
+    #"Host": "t.cn",
     "User-Agent": "Mozilla/5.0 (Linux; U; Android 4.4.4; Nexus 5 Build/KTU84P) AppleWebkit/534.30 "
                   "(KHTML, like Gecko) Version/4.0 Mobile Safari/534.30",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
@@ -49,6 +54,7 @@ async def fetch(client, semaphore, url, retry=1):
 
                     print("Success", url)
                     text = await  response.text()
+                    #print(text)
                     rgx = re.compile(r".+?video=\['(.+?)'\]", re.DOTALL)
                     result = re.match(rgx, text)
                     succ_url.append("http://zxfuli.h6080.com" + result.group(1))
@@ -62,11 +68,60 @@ async def fetch(client, semaphore, url, retry=1):
     except asyncio.TimeoutError:
         print(url, "Conntection time out", retry, "times")
         return await fetch(client, semaphore, url, retry+1)
+    except aiohttp.errors.ServerDisconnectedError:
+        err_url.append(url)
+        return
+
+
+async def download_one(client, semaphore, url, url_list, retry=1):
+    if retry > 5:
+        err_url.append(url)
+        return
+    try:
+        with await semaphore:
+            with aiohttp.Timeout(300):
+                print("geting url: ", url, retry, "times")
+                response = await client.get(url, headers=headers)
+                async with response:
+                    if response.status != 200:
+                        print("Error", url, "in", retry, "times", response.status)
+                        return await download_one(client, semaphore, url, url_list, retry+1)
+
+                    byte = await response.read()
+                    with open(str(url_list.index(url) + 50) + ".mp4", "wb") as f:
+                        f.write(byte)
+                    print("Success", url)
+                    succ_url.append(url)
+
+    except asyncio.TimeoutError:
+        print(url, "Conntection time out", retry, "times")
+        return await download_one(client, semaphore, url, url_list, retry+1)
+    except Exception:
+        err_url.append(url)
+        return
+
+
+async def download(loop, client, semaphore):
+    url_list = []
+    with open(fail_dir, "r") as f:
+        for line in f.readlines():
+            url_list.append(line.strip())
+
+    task_list = [loop.create_task(download_one(client, semaphore, url, url_list)) for url in url_list]
+    await  asyncio.wait(task_list)
+
+    with open(fail_dir, "w") as f:
+        global err_url
+        if err_url:
+            err_url = set(err_url)
+        for url in err_url:
+            f.write(url + "\n")
+    print("Scuuess %d. fail %d" % (len(set(succ_url)), len(err_url)))
+
 
 async def fetch_all(loop, client, semaphore):
     url_list = get_http_address()
-    test_list = url_list[:5]
-    task_list = [loop.create_task(fetch(client, semaphore, url)) for url in test_list]
+    task_list = [loop.create_task(fetch(client, semaphore, url)) for url in url_list]
     await asyncio.wait(task_list)
 
     with open(succ_dir, "a+") as f:
@@ -77,9 +132,10 @@ async def fetch_all(loop, client, semaphore):
             f.write(url + "\n")
 
 
+
+
 if __name__ == "__main__":
     semaphore = asyncio.Semaphore(CONCURRENT_REQUESTS)
     loop = asyncio.get_event_loop()
     with aiohttp.ClientSession(loop=loop) as client:
-        loop.run_until_complete(fetch_all(loop, client, semaphore))
-
+        loop.run_until_complete(download(loop, client, semaphore))
