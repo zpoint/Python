cce9f76ef23efdc391362ba896b1be75047fcccc
zpoint <zeping.guo@bit-beyond.com>
2016-12-13 18:44:46 +0800

    monitor with auto click notify button

--- teambition_auto_monitor/teambition.py
+++ teambition_auto_monitor/teambition.py
@@ -13,6 +13,18 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
+
+def button_wrapper(method):
+    def inner_helper(*args):
+        try:
+            method(*args)
+        except selenium.common.exceptions.NoSuchElementException:
+            args[0].click_notify_button()
+            args[0].loop()
+    return inner_helper
+
+
+DEFAULT_CLICK_TIME = 1.5
 class teambition(object):
     def __init__(self, userid, passwd, apiuserid, apipasswd):
         self.userid = userid
@@ -24,19 +36,21 @@ class teambition(object):
         self.unsolved_list = []
         self.api_login()  # login api page
         self.login()  # login teambition
-
         time.sleep(3)  # for login, the "wait until" in selenium has bug
         self.get_notify()  # find notify in DOM
         self.loop_time = 1
+
+    @button_wrapper
+    def loop(self):
         while True:
             self.get_api_list()  # refresh api list
-            self.click_down_notify(1.5)  # click down my task
+            self.click_down_notify(DEFAULT_CLICK_TIME)  # click down my task
             # filter the robot tasks
             for each_tasks in self.get_tasks():
                 self.parse_tasks_ul(each_tasks)
 
             self.recheck()  # check whether cause by timeout or not, if timeout, reset it
-            self.click_up_notify(1.5)  # click up for later refresh
+            self.click_up_notify(DEFAULT_CLICK_TIME)  # click up for later refresh
             self.loop_time += 1
             logging.info("%-4d times loops done" % (self.loop_time, ))
             time.sleep(random.randint(3, 10))  # sleep
@@ -80,21 +94,33 @@ class teambition(object):
     def click_down_notify(self, seconds):
         self.my_notify.click()
         time.sleep(seconds)
-        try:
-            self.get_tasks()
-        except selenium.common.exceptions.NoSuchElementException:
-            logging.warning("Error calling click_down_notify, seconds: " + str(seconds))
-            self.click_down_notify(seconds + 1)
+        if seconds < 4:
+            try:
+                self.get_tasks()
+            except selenium.common.exceptions.NoSuchElementException:
+                logging.warning("Error calling click_down_notify, seconds: " + str(seconds))
+                self.click_down_notify(seconds + 1)
+        else:
+            self.click_notify_button()
+            self.click_down_notify(DEFAULT_CLICK_TIME)
 
     def click_up_notify(self, seconds):
         self.my_notify.click()
         time.sleep(seconds)
-        try:
-            self.get_tasks()
-        except selenium.common.exceptions.NoSuchElementException:
-            return
-        logging.warning("Error click_up_notify, seconds: " + str(seconds))
-        self.click_up_notify(seconds + 1)
+        if seconds < 4:
+            try:
+                self.get_tasks()
+            except selenium.common.exceptions.NoSuchElementException:
+                return
+            self.click_up_notify(seconds + 1)
+        self.click_notify_button()
+        self.click_down_notify(DEFAULT_CLICK_TIME)
+
+    def click_notify_button(self):
+        xpath = "//button[@class='request-permission-btn btn btn-primary btn-block']"
+        button = self.driver.find_element_by_xpath(xpath)
+        print(button)
+        button.click()
 
     def get_tasks(self):
         xpaths = {
@@ -103,7 +129,7 @@ class teambition(object):
             "week_tasks": '//div[@class="week-tasks"]/ul[@class="task-list my-list"]'
         }
         today_tasks_ul = self.driver.find_element_by_xpath(xpaths["today_tasks"])
-        return (today_tasks_ul, )
+        return tuple(today_tasks_ul)
 
     def parse_tasks_ul(self, tasks):
         ls_items = tasks.find_elements_by_xpath('./li')
@@ -115,11 +141,25 @@ class teambition(object):
             if "ApiBugList_AUTO_TASK" in content_div.text:
                 self.unsolved_list.append((box, content_div))
 
-    def clock_box(self, solved_list):
-        pass
+    def click_box(self, solved_text_list):
+        for i in range(len(solved_text_list)):
+            each_text = solved_text_list[i]
+            today_tasks_ul = self.get_tasks()[0]
+            ls_items = today_tasks_ul.find_elements_by_xpath('./li')
+            try:
+                for each_li in ls_items:
+                    box = each_li.find_element_by_xpath('./a[@class="check-box"]')
+                    content_div = each_li.find_element_by_xpath('./div[@class="task-content-set"]')
+                    if content_div.text == each_text:
+                        box.click()
+                        logging.info("Teambition box click Done " + each_text)
+                        break
+            except selenium.common.exceptions.StaleElementReferenceException:
+                print("StaleElementReferenceException", solved_text_list[i])
+                self.click_box(solved_text_list[i:])
 
     def recheck(self):
-        solved_list = []
+        solved_text_list = []
         rgx = re.compile(r".+?(http://.+?)｜")
         for box, content_div in self.unsolved_list:
             url = re.match(rgx, content_div.text).group(1).strip()
@@ -130,13 +170,16 @@ class teambition(object):
             api = urllib.parse.urlparse(url).path[1:]
             if "data" in json_obj and len(json_obj["data"]) > 0:
                 logging.info(api + " behave correct")
+                solved_text_list.append(content_div.text)
                 for each_data in self.api_list_response:
                     if each_data["api"] == api and each_data["status"] == "0":
                         self.monitor(each_data)
-                        solved_list.append(content_div)
+                        each_data["status"] = "1"
                         break
             else:
                 logging.info(api + " behave error")
+        self.unsolved_list = []
+        self.click_box(solved_text_list)
 
     def api_login(self):
         payload = {
@@ -175,7 +218,13 @@ class teambition(object):
         url = "http://120.25.255.56/api/tableView"
         logging.info(url)
         response = requests.post(url, data=payload, headers=self.api_loin_headers)
-        self.api_list_response = json.loads(response.content.decode())["data"]
+        response_json = json.loads(response.content.decode())
+        if "error" in response_json:
+            print(response_json)
+            self.api_login()
+            self.get_api_list()
+        else:
+            self.api_list_response = response_json["data"]
 
 
     def monitor(self, dataobj):
@@ -187,4 +236,11 @@ class teambition(object):
         logging.info(dataobj["name"] + " " + url)
         response = requests.post(url, data=payload, headers=self.api_loin_headers)
         content = json.loads(response.content.decode())
-        assert content["error"] == 0
+        print(content)
+        if content["error"] != 0:
+            if content['msg'] == 'API测试失败':
+                logging.info(content)
+            else:
+                logging.info(content)
+                self.api_login()
+                self.monitor(dataobj)
